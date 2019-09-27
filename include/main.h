606b7ba736d9aeebafec2fa9e58da6c6dd3691fb
Param Deshpande <deshpandeparam10@gmail.com>
2019-09-27 22:24:16 +0530

    moved

--- include/main.h
+++ include/main.h
@@ -4,6 +4,6 @@
 /*THIS file contains all the info related to registers, timers, OSC freq ,typedefs and interrupts */
 
 /*Reg maps */
-#include "Arduino.h"
+#include "mbed.h"
     
 #endif // !MAIN_H
