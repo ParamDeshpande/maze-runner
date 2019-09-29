4e51aacf69367c3ced2803dcd99b2646b1a60da5
Param Deshpande <deshpandeparam10@gmail.com>
2019-09-29 19:13:58 +0530

    edited port.h

--- include/port.h
+++ include/port.h
@@ -8,13 +8,13 @@
 #define battery_Pin PC_4
 //Left motor Pin_Nos 
 #define rme PC_8
-#define rmi1 PC_9
-#define rmi2 PB_8       
+#define rmi1 PB_6
+#define rmi2 PC_7       
 
 //Right motor Pin_Nos
-#define lme PC_6
-#define lmi1 PA_5
-#define lmi2 PA_6
+#define lme PC_9
+#define lmi1 PA_6
+#define lmi2 PA_7
 
 
 //IR Pin_NoS will be fed to IR object
@@ -26,7 +26,7 @@
 #define diag_r_tx_Pin_No PB_5
 
 #define side_l_tx_Pin_No PH_1
-#define side_r_tx_Pin_No PB_3
+#define side_r_tx_Pin_No PA_10
 
 #define front_l_rx_Pin_No PC_2
 #define diag_l_rx_Pin_No PC_0
