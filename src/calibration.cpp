f68e8bada6ffd42c78ee983f54f60580f990f584
ParamDeshpande <deshpandeparam10@gmail.com>
2019-12-29 14:23:03 +0530

    tweaked calib.cpp

--- src/calibration.cpp
+++ src/calibration.cpp
@@ -3,11 +3,13 @@
 #include "buzzer.h"
 #include "actuator.h"
 #include "encoder.h"
+#include "Utils.h"
 
 #define DEBUG
 
-#define Stable_VARIANCE_YAW 50.0
-#define MOTOR_TURN_SPEED 20.0
+#define Stable_VARIANCE_YAW 100.0
+#define MOTOR_TURN_SPEED 10.0
+#define STABLE_BUF_SIZE 150
 
 //PRIVATE FUNCTIONS 
 int variance(int [], int); 
@@ -35,36 +37,36 @@ void self_calib_IMU(void){
    
     //Check if stable readings ?
 
-    int value_buffer[10] = {0};
-    for (int i = 0; i < 10; i++)
-    {
+    int value_buffer[STABLE_BUF_SIZE] = {0};
+    for (int i = 0; i < STABLE_BUF_SIZE; i++){
         refresh_imu();
         value_buffer[i] = yaw;
     }
     #ifdef DEBUG
+    int test_variance = variance(value_buffer, STABLE_BUF_SIZE);
     pc.printf("the current variance is %d", variance(value_buffer, 10));
     #endif // DEBUG
     //Get the variance if stable move on.
 
-    if(variance(value_buffer, 10) < Stable_VARIANCE_YAW){
-    buzzer_off();
-    yaw_offset = yaw;
-    bool turn_90degs = false;
-    while (turn_90degs != true )
-    {
-         
+    if(variance(value_buffer, STABLE_BUF_SIZE) < Stable_VARIANCE_YAW){
+        buzzer_off();
+        yaw_offset = yaw;
+        bool turn_90degs = false;
+        while (turn_90degs != true )
+        {
+        feed_enc();    
         //Turn the bot by +90degs.
-        if(L_enc_position < 45*ONE_DEG_YAW_ENC_COUNT){
+        if(L_enc_position < 45 *ONE_DEG_YAW_ENC_COUNT){
             l_forward(MOTOR_TURN_SPEED);
         }
-        else
+        else if(L_enc_position > 45*ONE_DEG_YAW_ENC_COUNT)
         {
-            l_forward(0);
+          l_forward(0);
         }
-        if(R_enc_position > (-45*ONE_DEG_YAW_ENC_COUNT)){
+        if((-R_enc_position) < (45*ONE_DEG_YAW_ENC_COUNT)){
             r_backward(MOTOR_TURN_SPEED);   
         }
-        else
+        else if((-R_enc_position) > (45*ONE_DEG_YAW_ENC_COUNT))
         {
             r_backward(0);
         }
@@ -72,44 +74,47 @@ void self_calib_IMU(void){
         {
             turn_90degs = true;
         }
+        }
+        // Now it has turned 90 degs
+        refresh_imu();
+        wait(0.5);
+        imu_calib_factor = (90)/(yaw - yaw_offset);
+        pc.printf("The mult factor is %lf ", imu_calib_factor);
+    
         
-        
-    }
-    // Now it has turned 90 degs.
-    imu_calib_factor = (90)/(yaw - yaw_offset);
-
-
-    //Now turn it back to -180 degs
-    bool turn_back_180degs = false;
-    while (turn_back_180degs != true )
-    {   
-        if(L_enc_position > (-90*ONE_DEG_YAW_ENC_COUNT)){
+        //Now turn it back to -180 degs
+        bool turn_back_180degs = false;
+        while (turn_back_180degs != true )
+        {   
+        feed_enc();
+        if(L_enc_position > (-45*ONE_DEG_YAW_ENC_COUNT)){
             l_backward(MOTOR_TURN_SPEED);
         }
         else
         {
             l_forward(0);
         }
-        if(R_enc_position < (90*ONE_DEG_YAW_ENC_COUNT)){
+        if(R_enc_position < (45*ONE_DEG_YAW_ENC_COUNT)){
             r_forward(MOTOR_TURN_SPEED);   
         }
         else
         {
             r_backward(0);
         }
-        if ((L_enc_position <= (-90*ONE_DEG_YAW_ENC_COUNT)) && (R_enc_position >= (90*ONE_DEG_YAW_ENC_COUNT)))
+        if ((L_enc_position <= (-45*ONE_DEG_YAW_ENC_COUNT)) && (R_enc_position >= (45*ONE_DEG_YAW_ENC_COUNT)))
         {
             turn_back_180degs = true;
         }
 
-    }
-
-    // Has turned backed 180 degs 
-    //Recheck if calib factor gives correct readings 
-    // ie if calibration is successful 
-    float new_angle = imu_calib_factor*(yaw - yaw_offset); 
-    if ((new_angle < -60) && (new_angle > -120))
-    {
+        }
+    
+        // Has turned backed 180 degs 
+        //Recheck if calib factor gives correct readings 
+        // ie if calibration is successful 
+        refresh_imu();
+        float new_angle = imu_calib_factor*(yaw - yaw_offset); 
+        if ((new_angle < -60) && (new_angle > -120))
+        {
         //victory beep
         buzzer_on();
         wait_ms(300);
@@ -121,41 +126,35 @@ void self_calib_IMU(void){
         buzzer_off();
         wait_ms(300);
         
-    }
-
-    bool turn_back_0degs = false;
-    while (turn_back_0degs != true )
-    {
-        
-        //Turn the bot by +90degs back to zero degs.
-        if(L_enc_position < 0.0){
-            l_forward(MOTOR_TURN_SPEED);
-        }
-        else
-        {
-            l_forward(0);
-        }
-        if(R_enc_position > 0.0){
-            r_backward(MOTOR_TURN_SPEED);   
-        }
-        else
-        {
-            r_backward(0);
-        }
-        if ((L_enc_position >= 0.0) && (R_enc_position <= 0.0))
-        {
-            turn_90degs = true;
         }
         
-        #ifdef DEBUG
-        while (1)
-        {
-        pc.printf("the var is %d, the bias is %lf, the mult_factor is %lf \n \r ", variance(value_buffer, 10), yaw_offset, imu_calib_factor);
+        bool turn_back_0degs = false;
+        while (turn_back_0degs != true )
+        {   
+            feed_enc();
+            if(L_enc_position < (0*ONE_DEG_YAW_ENC_COUNT)){
+                l_forward(MOTOR_TURN_SPEED);
+            }
+            else
+            {
+                l_forward(0);
+            }
+            if(R_enc_position > (0*ONE_DEG_YAW_ENC_COUNT)){
+                r_backward(MOTOR_TURN_SPEED);   
+            }
+            else
+            {
+                r_backward(0);
+            }
+            if ((L_enc_position >= (0*ONE_DEG_YAW_ENC_COUNT)) && (R_enc_position <= (0*ONE_DEG_YAW_ENC_COUNT)))
+            {
+                turn_back_180degs = true;
+            }
+
+            #ifdef DEBUG
+            pc.printf("The yaw offset is %lf ,and muly fact is %lf", yaw_offset, imu_calib_factor );
+            #endif // DEBUG
         }
-        #endif // DEBUG
-    }
-
-
     }
     else
     {
